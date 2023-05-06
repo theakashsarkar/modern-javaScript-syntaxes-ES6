a41e923bbf29e3e15a1632d1cfc9a710974c10fe
Ashraful islam <theakashsarkar7@gmail.com>
2023-05-06 08:43:56 +0600

    javaScript fat arrow function

--- arrowFunction.js
+++ arrowFunction.js
@@ -2,6 +2,25 @@
 //     return 10;
 // }
 
-let number = (a, b) => a + b;
+// let number = (a, b) => a + b;
 
-console.log(number(10, 5));
+// console.log(number(10, 5));
+
+var javascript = {
+    name :'JavaScript',
+    libraries: ['react', 'vue', 'angular'],
+    printLibraries: function() {
+        this.libraries.forEach((a) => {
+            console.log(`${this.name} love ${a}`);
+        })
+    }
+}
+
+javascript.printLibraries();
+
+
+function persone(name) {
+    this.name = name;
+}
+
+var sakib = new persone('sakib');
