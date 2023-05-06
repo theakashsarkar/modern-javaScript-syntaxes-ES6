092f4354497ee46ea1e0ba5e13250023f7664ad7
Ashraful islam <theakashsarkar7@gmail.com>
2023-05-06 09:07:50 +0600

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,11 @@
+var age = 18;
+
+var type = (age >= 18) ? "adult" : (age < 10) ? "child" : "Young";
+console.log(type);
+
+// if (age < 18)
+// {
+//     type = 'adult';
+// } else {
+//     type = "child";
+// }
